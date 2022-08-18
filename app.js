67ea6edd689fafff230da3d0dc9ce77b3cfd5d17
YotamBG <yotam.bengera@gmail.com>
2022-08-18 15:19:08 +0300

    Plan API endpoints

--- app.js
+++ app.js
@@ -19,3 +19,26 @@ app.get('/:id', (req, res, next) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
+
+// GET, POST, PUT, DELETE
+
+// /products (get all)
+// /products/new-product
+// /products/update-product/:productId
+// /products/delete-product/:productId
+
+
+// /users/register
+// /users/login -> return cartId (to be saved in local storage)
+// /users/logout -> delete all related carts
+// /users/:username/update
+// /users/:username/delete -> delete all related carts
+// /users/:username/profile
+
+
+// /carts/:cartId/new-item/:productId
+// /carts/:cartId/remove-item/:productId
+// /carts/:cartId/clear-cart
+
+// /orders/newOrder/:cartId -> delete cart

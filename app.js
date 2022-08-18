5b37d94807d200c7c56c5497e555c306504afe2d
YotamBG <yotam.bengera@gmail.com>
2022-08-18 16:57:42 +0300

    Set up user registration

--- app.js
+++ app.js
@@ -2,23 +2,49 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome to the e-commerce application!');
 });
 
-app.get('/:id', (req, res, next) => {
-  db.query('SELECT * FROM users WHERE user_id = $1', [req.params.id], (err, result) => {
-    if (err) {
-      return next(err);
-    }
-    res.send(result.rows[0]);
-  })
-})
+// app.get('/:id', (req, res, next) => {
+//   db.query('SELECT * FROM users WHERE user_id = $1', [req.params.id], (err, result) => {
+//     if (err) {
+//       return next(err);
+//     }
+//     res.send(result.rows[0]);
+//   })
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
+
+app.post('/users/register', (req, res, next) => {
+  const { username, password } = req.body;
+  db.query('SELECT * FROM users WHERE username = $1', [username], (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    if (result.rows.length == 0) {
+      db.query('INSERT INTO users (username, password) VALUES ($1, $2);', [username, password], (err, result) => {
+        if (err) {
+          return next(err);
+        }
+        res.send('success!');
+      })
+    } else {
+      res.send('username already used!');
+    }
+  })
+});
+
 
 
 // GET, POST, PUT, DELETE

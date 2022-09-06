90f56d42671109749c8a2ac89f499309b3d180af
YotamBG <yotam.bengera@gmail.com>
2022-09-06 22:07:58 +0300

    var config2

--- utils/db.js
+++ utils/db.js
@@ -4,13 +4,10 @@ const devConfig = `postgresql://postgres:postgres@localhost:5432/E-Commerce-App`
 
 const proConfig = process.env.DATABASE_URL; //heroku addons
 
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
-  ssl: {
-    rejectUnauthorized: false // don't check for SSL cert
-  }
-});
+const options = process.env.NODE_ENV === "production" ? {connectionString: proConfig, ssl: 
+  {rejectUnauthorized: false}} : {connectionString: devConfig};
+
+const pool = new Pool(options);
 
 module.exports = {
   query: (text, params, callback) => {

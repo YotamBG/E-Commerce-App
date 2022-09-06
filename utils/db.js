5ad3155192ecae963292643f41414ef7ed9d5355
YotamBG <yotam.bengera@gmail.com>
2022-09-06 18:02:59 +0300

    config2

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-    // user: "postgres",
-    // password: "postgres",
-    // host: "localhost",
-    // port: 5432,
-    // database: "E-Commerce-App",
+  connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
+  ssl: {
+    rejectUnauthorized: false // don't check for SSL cert
+  }
+  // user: "postgres",
+  // password: "postgres",
+  // host: "localhost",
+  // port: 5432,
+  // database: "E-Commerce-App",
 });
 
 module.exports = {
